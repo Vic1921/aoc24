c90ca6d918501fa8a0ef10d5c6e57ec0430690b3
Victor Binzar <a12022144@unet.univie.ac.at>
2024-12-02 13:27:49 +0200

    finished day 1

--- src/main/java/day1/MainDay1.java
+++ src/main/java/day1/MainDay1.java
@@ -1,18 +1,21 @@
-package noorg.vic;
+package day1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
-public class Main {
+public class MainDay1 {
     public static void main(String[] args) {
         try {
-            File file = new File("src/main/java/noorg/vic/day1/input.txt");
+            File file = new File("src/main/java/day1/input.txt");
             Scanner scanner = new Scanner(file);
 
             // Read values into an array
-            int[] values = new int[2000]; // Replace 100 with the actual number of values
+            int[] values = new int[2000];
             int index = 0;
             while (scanner.hasNextInt()) {
                 values[index++] = scanner.nextInt();
@@ -35,7 +38,6 @@ public class Main {
                 }
             }
 
-
             // Sort the array
             Arrays.sort(firstHalf);
             Arrays.sort(secondHalf);
@@ -50,6 +52,25 @@ public class Main {
                 sum += Math.abs(firstHalf[i] - secondHalf[i]);
             }
             System.out.println("The sum is: " + sum);
+
+            // PART 2
+            int similarityScore = 0; // This has to be added up following the multiplication of appearances
+
+            // Convert arrays to List so that they are streamable
+            ArrayList<Integer> firstHalfList = new ArrayList<>();
+            ArrayList<Integer> secondHalfList = new ArrayList<>();
+            IntStream.of(firstHalf).forEach(firstHalfList::add);
+            IntStream.of(secondHalf).forEach(secondHalfList::add);
+
+
+            // Multiply the integers in the first half List by the number of duplicates in the second half List
+            // add the result to the similarity score
+            similarityScore = firstHalfList.stream()
+                    .mapToInt(firstHalfInt -> firstHalfInt * Collections.frequency(secondHalfList, firstHalfInt))
+                    .sum();
+
+            System.out.println("The similarity score is: " + similarityScore);
+
 
         } catch (FileNotFoundException e) {
             System.out.println("File not found.");
