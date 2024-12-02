c49a8fe7c7fd285a36b6c80c66d69df044a127ec
Victor Binzar <a12022144@unet.univie.ac.at>
2024-12-02 12:33:58 +0200

    finished part 1 of day 1

--- src/main/java/noorg/vic/Main.java
+++ src/main/java/noorg/vic/Main.java
@@ -1,19 +1,58 @@
 package noorg.vic;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        try {
+            File file = new File("src/main/java/noorg/vic/day1/input.txt");
+            Scanner scanner = new Scanner(file);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+            // Read values into an array
+            int[] values = new int[2000]; // Replace 100 with the actual number of values
+            int index = 0;
+            while (scanner.hasNextInt()) {
+                values[index++] = scanner.nextInt();
+            }
+            scanner.close();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+            // Split the 2 arrays
+            // first array takes all the even values
+            // second array takes in all the odd values
+
+            int[] firstHalf = new int[values.length / 2];
+            int[] secondHalf = new int[values.length / 2];
+            int firstHalfIndex = 0;
+            int secondHalfIndex = 0;
+            for (int i = 0; i < values.length; i++) {
+                if (i % 2 == 0) {
+                    firstHalf[firstHalfIndex++] = values[i];
+                } else {
+                    secondHalf[secondHalfIndex++] = values[i];
+                }
+            }
+
+
+            // Sort the array
+            Arrays.sort(firstHalf);
+            Arrays.sort(secondHalf);
+
+            // Print for test both
+            System.out.println("This is the first half: " + Arrays.toString(firstHalf));
+            System.out.println("This is the second half: " + Arrays.toString(secondHalf));
+
+            // Sum the difference between the 2 arrays
+            int sum = 0;
+            for (int i = 0; i < firstHalf.length; i++) {
+                sum += Math.abs(firstHalf[i] - secondHalf[i]);
+            }
+            System.out.println("The sum is: " + sum);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
         }
     }
 }
